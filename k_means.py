392cf06e57f922c035e9db996338db4e1f579329
natanjmai <natan.mai@hotmail.com>
2015-08-12 00:14:51 -0300

    K-means

---
+++ k_means.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @NatanJMai - natan.mai@hotmail.com
+
+from Fish   import Fish
+from task01 import taskMain
+from enum   import Enum
+import math
+
+class Cluster(Enum):
+  Robalo = 0
+  Salmao = 1
+  Nada   = 2
+
+def main(p1, p2):
+  stop = True
+
+  while stop:
+    print(p1[0])
+    stop = False
+  opa = taskMain()
+
+
+def passo1():
+  return True
+
+def passo2():
+  return True
+
+def passo3():
+  return True
+
+def passo4():
+  return True
+
+def passo5():
+  return True
+
+
+if __name__ == "__main__":
+  p1 = (2, 3)
+  p2 = (3, 4)
+  main(p1, p2)
