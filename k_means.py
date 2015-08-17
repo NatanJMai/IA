ba8e6fc570a0a444c6d2f0976d7b501fa9548c51
natanjmai <natan.mai@hotmail.com>
2015-08-17 19:06:44 -0300

    Update final

--- k_means.py
+++ k_means.py
@@ -6,9 +6,6 @@ from task01 import taskMain
 from enum   import Enum
 from math import sqrt
 
-global i
-i = 0
-
 class Type(Enum):
   Robalo = 0
   Salmao = 1
@@ -23,12 +20,10 @@ class Cluster(object):
 
 
 def main(p1, p2):
-  stop = True
   n    = 0
   j    = 0
-  while n < 100 and j < 10:
-    passo2()
-    if i == 1:
+  while n < 100 and j < 100:
+    if passo2() == 1:
       j += 1
     passo3()
     n += 1
@@ -38,24 +33,14 @@ def main(p1, p2):
   for it in setFish:
     if it.tipo == "A":
       nr1 += 1
-      print(it.light, it.length)
-
-  print(nr1)
+      print("%f - %f" % (it.light, it.length))
 
   nr = 0
   print("B ===========> ")
   for it in setFish:
     if it.tipo == "B":
       nr += 1
-      print(it.light, it.length)
-
-  print(nr)
-  print(len(setFish), nr + nr1)
-#  for i in clusterB.setC:
- #   print(i.light, i.length)
-
-#    print("A => %f - %f" % (clusterA.center[0], clusterA.center[1]))
-#    print("B => %f - %f" % (clusterB.center[0], clusterB.center[1]))
+      print("%f - %f" % (it.light, it.length))
 
 
 def getDistances(fish, cluster):
@@ -71,68 +56,59 @@ def passo1(p1, p2):
   passo2()
 
 def passo2():
+  i = 0
   for it in setFish:
     it.distA = getDistances(it, clusterA.center)
     it.distB = getDistances(it, clusterB.center) 
 
     if it.distA > it.distB:
       if it.tipo == "A":
-        print("s")
         i = 1
       it.tipo = "B"
     else:
       if it.tipo == "B":
-        print("a")
         i = 1
       it.tipo = "A"
-
-#  passo3()
+  return i
 
 def passo3():
-  #novos centroides
-  
   totalL = 0
   totalC = 0
   nr     = 0
+  a      = 0
+  b      = 0
   for i in setFish:
     if i.tipo == "A":
       nr += 1
       totalL += i.light
       totalC += i.length
-
-  a = totalL / nr
-  b = totalC / nr
-  clusterA.center = (a, b)
+  
+  if nr != 0:
+    a = totalL / nr
+    b = totalC / nr
+    clusterA.center = (a, b)
 
   totalL = 0
   totalC = 0
   nr     = 0
+  a      = 0
+  b      = 0
   for i in setFish:
     if i.tipo == "B":
       nr += 1
       totalL += i.light
       totalC += i.length
 
-  a = totalL / nr
-  b = totalC / nr
+  if nr != 0:
+    a = totalL / nr
+    b = totalC / nr
   
-  clusterB.center = (a, b)
-  
-#  print("------------------------------------------------\n")
-#  print(clusterA.center)
-#  print(clusterB.center)
-
-
-def passo4():
-  return True
-
-def passo5():
-  return True
+    clusterB.center = (a, b)
 
 
 if __name__ == "__main__":
-  p1 = (1.0, 14)
-  p2 = (1, 17)
+  p1 = (1.4, 21)
+  p2 = (10.2, 17)
   
   global clusterA
   global clusterB
